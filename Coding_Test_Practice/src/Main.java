c3b35627c5042586248c9a514f66bfe9c2e783a5
kros235 <kros235@naver.com>
2022-09-14 18:04:57 +0900

    2022-09-14
    onGoing etc question

--- Coding_Test_Practice/src/Main.java
+++ Coding_Test_Practice/src/Main.java
@@ -1,19 +1,60 @@
 import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static long find_profit_point(long fixed_budget, long manufacturing_budget, long sales_price) {
-        long investments, differ, decrease_amount;
-        if (manufacturing_budget >= sales_price)
-            return -1;
-        else if( ( fixed_budget + manufacturing_budget ) < sales_price)
-            return 1;
-        else {
-            investments = fixed_budget + manufacturing_budget;
-            differ = investments - sales_price;
-            decrease_amount = sales_price - manufacturing_budget;
+    public static ArrayList<Integer> get_pattern_cycle(int a) {
 
-            return 1 + (differ / decrease_amount) + 1;
+        ArrayList<Integer> pattern = new ArrayList<>();
+        boolean exist_checker = false;
+
+        int remainder;
+
+        for( int i = 1 ; ; i++){
+
+            remainder = (int)Math.pow ( a, i ) % 10;
+
+            for (int j = 0 ; j < pattern.size() ; j++){
+                if( pattern.get(j) == remainder) {
+                    exist_checker = true;
+                    break;
+                }
+            }
+
+            if (exist_checker == true)
+                break;
+            else
+                pattern.add ( remainder );
+        }
+
+        return pattern ;
+    }
+
+    public static int check_processed_comNo(int a, int b) {
+
+        ArrayList<Integer> pattern = new ArrayList<>();
+
+
+
+        pattern = get_pattern_cycle( a );
+
+
+        for(int i = 0 ; i < pattern.size() ; i ++)
+            System.out.print (pattern.get(i) + " ");
+        System.out.print ("\n");
+
+        if( a < 1 || a> 100 || b < 1 || b > 1000000)
+            return -1 ;
+        else{
+
+
+
+                System.out.println ("pattern.size() = " + pattern.size());
+                System.out.println ("Return = " + pattern.get( (pattern.size()) - (b % pattern.size()) ) );
+                return pattern.get( (pattern.size()) - (b % pattern.size()) ) ;
+
+
         }
     }
 
@@ -22,22 +63,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String input = br.readLine();
-        String[] input_digits = input.split(" ");
+        String round = br.readLine();
+        int a, b;
+        int processed_comNo;
+        ArrayList<Integer> result = new ArrayList<>();
 
-        long fixed_budget = Long.parseLong(input_digits[0]);
-        long manufacturing_budget = Long.parseLong(input_digits[1]);
-        long sales_price = Long.parseLong(input_digits[2]);
+        for ( int i = 0 ; i < Integer.parseInt( round ) ; i ++ ){
+            String input = br.readLine();
+            String[] input_digits = input.split(" ");
 
+            a = Integer.parseInt( input_digits[0] );
+            b = Integer.parseInt( input_digits[1] );
 
-        if (fixed_budget < 0 || manufacturing_budget < 0 || sales_price < 0
-                || fixed_budget > 2100000000 || manufacturing_budget > 2100000000 || sales_price > 2100000000) {
-        } else {
-
-            long result_count = find_profit_point(fixed_budget, manufacturing_budget, sales_price);
-            bw.write(String.valueOf(result_count));
-            bw.flush();
+            processed_comNo = check_processed_comNo( a, b );
+            if( processed_comNo != -1  )
+                result.add ( processed_comNo );
         }
+
+        for ( int i = 0 ; i < result.size() ; i ++ ) {
+            bw.write(String.valueOf(result.get(i)));
+            if ( i != result.size() -1 )
+                bw.write("\n");
+        }
+
+        bw.flush();
         br.close();
         bw.close();
     }
