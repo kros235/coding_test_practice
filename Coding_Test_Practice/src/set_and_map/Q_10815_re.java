3fc0a6a7330e5bd01a881c41ef199f7a16d90103
kros235 <kros235@naver.com>
2022-10-15 23:35:16 +0900

    2022-10-15
    Chapter #12 :
     - set and map

---
+++ Coding_Test_Practice/src/set_and_map/Q_10815_re.java
@@ -0,0 +1,31 @@
+package set_and_map;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+public class Q_10815_re {
+
+    public static void main(String args[]) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<String> hs = new HashSet<String>();
+        while (st.hasMoreTokens()) hs.add(st.nextToken());
+        br.readLine();
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens())
+            if (hs.contains(st.nextToken())) bw.write("1 ");
+            else bw.write("0 ");
+        bw.newLine();
+        bw.close();
+        br.close();
+
+
+    }
+}
