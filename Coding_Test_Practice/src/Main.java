a3e39c3b9e3e746937de34acc6a7df1411dd418e
kros235 <kros235@naver.com>
2022-10-15 22:09:15 +0900

    2022-10-15
    before un pushed ones

--- Coding_Test_Practice/src/Main.java
+++ Coding_Test_Practice/src/Main.java
@@ -6,41 +6,20 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    public static int test( int a, int b){
+        return a > b  ? a : b;
+    }
+
     public static void main(String args[]) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = 3;
+        int b = 2;
+        int result = test ( a,  b);
+
+        System.out.println(result);
 
 
-        int amount              =   Integer.parseInt( br.readLine() );
-        int[][] coordinate_list   =   new int[amount][2];
 
-        StringTokenizer st;
-        for ( int i = 0 ; i < amount ; i ++ ) {
-
-            st = new StringTokenizer( br.readLine() );
-
-            coordinate_list[i][0] = Integer.parseInt( st.nextToken() );
-            coordinate_list[i][1] = Integer.parseInt( st.nextToken() );
-        }
-
-        Arrays.sort(coordinate_list, (e1, e2) -> {
-            if (e1[0] == e2[0]) {
-                return e1[1] - e2[1];
-            }
-            else {
-                return e1[0] - e2[0];
-            }
-        });
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0 ; i < amount ; i++)
-            sb.append ( coordinate_list[i][0] + " " + coordinate_list[i][1] + "\n");
-
-        bw.write( String.valueOf(sb) );
-
-        bw.flush();
-        br.close();
-        bw.close();
     }
 }
