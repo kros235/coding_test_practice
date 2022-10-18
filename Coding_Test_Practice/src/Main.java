ae08f48ecc74a1587b4d6bfb4e9ced93fe59d786
kros235 <kros235@naver.com>
2022-10-18 22:28:23 +0900

    2022-10-18
    print_start working source code

--- Coding_Test_Practice/src/Main.java
+++ Coding_Test_Practice/src/Main.java
@@ -6,17 +6,58 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int counter = 0;
+    static String[][] result;
 
-    public static int recursion(String s, int l, int r){
+    public static void draw_square(int amount, int startX, int startY, boolean check_blank) {
+
+        int block_Size = amount / 3;
+        int counter = 0;
+
+        if (block_Size == 1 && check_blank == true) {
+            for (int i = startY; i < startY + amount; i++) {
+                for (int j = startX; j < startX + amount; j++) {
+                    if (counter == 5 * block_Size - 1) {
+                        result[i][j] = " ";
+                    } else
+                        result[i][j] = "*";
+                    counter++;
+                }
+            }
+        }
+        else if (check_blank == false){
+            for (int i = startY; i < startY + amount; i++) {
+                for (int j = startX; j < startX + amount; j++) {
+                        result[i][j] = " ";
+                }
+            }
+        }
+        else{
+            for (int i = startY; i < amount; i+= block_Size) {
+                for (int j = startX; j < amount; j+=block_Size) {
+
+                    if (counter == 5 - 1) {
+                        draw_square(amount/3, i, j, false);
+                    } else
+                        draw_square(amount/3, i, j, true);
+                    counter++;
+                }
+            }
+        }
+
+
+
+        /*
         counter ++;
         if(l >= r) return 1;
         else if(s.charAt(l) != s.charAt(r)) return 0;
         else return recursion(s, l+1, r-1);
+        */
     }
-    public static int isPalindrome(String s){
-        return recursion(s, 0, s.length()-1);
-    }
+
+        public static int log3(int amount) {
+            return (int) (Math.log(amount) / Math.log(3));
+        }
+
 
 
     public static void main(String args[]) throws IOException {
@@ -24,18 +65,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int amount = Integer.parseInt( br.readLine() );
+        int amount      =   Integer.parseInt( br.readLine() );
+        int exponent    =   log3(amount);
 
-        if ( amount >= 1 && amount <= 1000){
+        if ( exponent >= 1 && exponent < 8){
 
-            ArrayList<String> word_list = new ArrayList<>();
+            result = new String[amount][amount];
 
-            for ( int i = 0 ; i < amount ; i++ )
-                word_list.add( br.readLine() );
+            draw_square( amount, 0,0 , true);
 
-            for ( int i = 0 ; i < amount ; i++ ) {
-                bw.write(isPalindrome(word_list.get(i)) + " " + counter + "\n");
-                counter = 0;
+            for ( int i = 0 ; i < result.length ; i++ ) {
+                for ( int j = 0 ; j < result[i].length ; j++ )
+                    bw.write(result[i][j]);
+                bw.write("\n");
             }
         }
 
