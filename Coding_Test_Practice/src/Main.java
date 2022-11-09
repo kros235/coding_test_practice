41741cd3f01cd1ce694525a4cb3d83e4ed18e40b
kros235 <kros235@naver.com>
2022-11-09 14:06:06 +0900

    2022-11-09
    Chapter # etc :
     - etc

--- Coding_Test_Practice/src/Main.java
+++ Coding_Test_Practice/src/Main.java
@@ -1,28 +1,105 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
+
+    static int count = 0;
+
+
+    public static void move_plate(ArrayList<Integer> first_poll, ArrayList<Integer> second_poll, ArrayList<Integer> third_poll){
+
+        // finished
+        if ( first_poll.size() == 0 && second_poll.size() == 0) return;
+
+
+        else if ( first_poll.size()!= 0 ){
+
+            // first_poll -> third_poll
+            if ( third_poll.size() == 0
+                    || third_poll.get(third_poll.size()-1) > first_poll.get(first_poll.size()-1) ){
+                third_poll.add( 0, first_poll.get( first_poll.size()-1 ));
+                first_poll.remove( first_poll.size()-1 );
+                count ++;
+                move_plate( first_poll, second_poll, third_poll );
+            }
+
+
+            // first_poll -> second_poll
+            else
+                /*if ( third_poll.get(third_poll.size()-1) < first_poll.get(first_poll.size()-1)
+                    && ( second_poll.size() == 0 || second_poll.get(second_poll.size()-1) > first_poll.get(first_poll.size()-1) ) )*/
+            {
+                second_poll.add( 0, first_poll.get( first_poll.size()-1 ));
+                first_poll.remove( first_poll.size()-1 );
+                move_plate( first_poll, second_poll, third_poll );
+            }
+        }
+
+        else if ( second_poll.size()!= 0 ){
+
+            // second_poll -> third_poll
+            if (second_poll.get(second_poll.size() - 1) < third_poll.get(third_poll.size() - 1)) {
+                third_poll.add(0, second_poll.get(second_poll.size() - 1));
+                second_poll.remove(second_poll.size() - 1);
+                move_plate(first_poll, second_poll, third_poll);
+            }
+
+            // second_poll -> first_poll
+            else
+                /*if (second_poll.get(second_poll.size() - 1) < first_poll.get(first_poll.size() - 1))*/
+            {
+                first_poll.add(0, second_poll.get(second_poll.size() - 1));
+                second_poll.remove(second_poll.size() - 1);
+                move_plate(first_poll, second_poll, third_poll);
+            }
+
+
+        }
+
+
+        else{
+
+            // third_poll -> second_poll
+            if (third_poll.get(second_poll.size() - 1) < second_poll.get(third_poll.size() - 1))
+            {
+                second_poll.add(0, third_poll.get(third_poll.size() - 1));
+                third_poll.remove(third_poll.size() - 1);
+                move_plate(first_poll, second_poll, third_poll);
+            }
+
+            // third_poll -> first_poll
+            else {
+                first_poll.add(0, third_poll.get(third_poll.size() - 1));
+                third_poll.remove(third_poll.size() - 1);
+                move_plate(first_poll, second_poll, third_poll);
+            }
+        }
+
+    }
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringBuilder sb    =   new StringBuilder();
-        String input        =   br.readLine();
+        int plate = Integer.parseInt( br.readLine() );
 
-        for ( int i = 0 ; i < input.length() ; i++ ){
-            char partial    =   input.charAt(i);
-            if ( partial >= 65 && partial <= 90)
-                partial += 32;
-            else
-                partial -= 32;
-            sb.append(partial);
-        }
+        ArrayList<Integer> first_poll   =   new ArrayList<>();
+        ArrayList<Integer> second_poll  =   new ArrayList<>();
+        ArrayList<Integer> third_poll   =   new ArrayList<>();
 
-        bw.write( String.valueOf(sb) );
+        // fill_poll with plates
+        for ( int i = plate ; i > 0 ; i-- )
+            first_poll.add( i );
+
+        move_plate( first_poll, second_poll, third_poll );
+
+        bw.write( String.valueOf(count) );
 
         bw.flush();
         br.close();
         bw.close();
     }
+
+
 }
 
---
+++ Coding_Test_Practice/src/etc/Q_15733.java
@@ -0,0 +1,20 @@
+package etc;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q_15733 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write( "I'm Sexy" );
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
+
