68ab4cd2ecabe375d851cbcb9610d4b8fb9b53c1
kros235 <kros235@naver.com>
2022-11-14 23:26:32 +0900

    2022-11-14
    Chapter # etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_13277.java
+++ Coding_Test_Practice/src/etc/Q_13277.java
@@ -1,11 +1,12 @@
+package etc;
+
 import java.io.*;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Q_13277 {
 
     public static void main(String args[]) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -22,5 +23,5 @@ public class Main {
         br.close();
         bw.close();
     }
-}
 
+}
