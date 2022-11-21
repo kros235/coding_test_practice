48c4727c272909fc0fee8652cb3a074db3e36212
kros235 <kros235@naver.com>
2022-11-21 12:11:57 +0900

    2022-11-21
    Chapter # etc :
     - etc

--- Coding_Test_Practice/src/Main.java
+++ Coding_Test_Practice/src/Main.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
 public class Main {
 
     public static void main(String args[]) throws IOException {
@@ -6,11 +9,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int original_prize = Integer.parseInt( br.readLine() );
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
 
         StringBuilder sb = new StringBuilder();
-        sb.append( (int)(original_prize * 0.78) + " " + (int)(original_prize * 0.8 + ( (original_prize * 0.2) * 0.78 ) ) );
+        sb.append((a.add(b).multiply(a.subtract(b))));
         bw.write(String.valueOf(sb));
+
 
         bw.flush();
         br.close();
