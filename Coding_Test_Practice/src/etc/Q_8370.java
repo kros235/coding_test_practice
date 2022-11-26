9cb5b7570065b386fa8fd29fe5e3e28d0ac15567
kros235 <kros235@naver.com>
2022-11-27 00:51:29 +0900

    2022-11-27
    Chapter # etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_8370.java
+++ Coding_Test_Practice/src/etc/Q_8370.java
@@ -1,9 +1,11 @@
+package etc;
+
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Q_8370 {
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -23,6 +25,7 @@ public class Main {
         bw.flush();
         br.close();
         bw.close();
+
     }
 }
 
