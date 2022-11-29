c38d8748e288dcf0be25f599d1ab7cda97f975e8
kros235 <kros235@naver.com>
2022-11-30 00:36:11 +0900

    2022-11-30
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_8545.java
+++ Coding_Test_Practice/src/etc/Q_8545.java
@@ -1,9 +1,10 @@
+package etc;
+
 import java.io.*;
 
-public class Main {
+public class Q_8545 {
 
     public static void main(String args[]) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -19,6 +20,7 @@ public class Main {
         bw.flush();
         br.close();
         bw.close();
-    }
-}
 
+    }
+
+}
