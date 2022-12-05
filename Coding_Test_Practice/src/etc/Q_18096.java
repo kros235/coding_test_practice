84f7ca9c41809850dbc4309b68de47dd3d644b32
kros235 <kros235@naver.com>
2022-12-05 11:19:00 +0900

    2022-12-05
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_18096.java
+++ Coding_Test_Practice/src/etc/Q_18096.java
@@ -1,9 +1,12 @@
-import java.io.*;
+package etc;
 
-public class Main {
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Q_18096 {
 
     public static void main(String args[]) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -16,7 +19,6 @@ public class Main {
         bw.flush();
         br.close();
         bw.close();
-
     }
-}
 
+}
