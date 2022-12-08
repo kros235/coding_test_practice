0511b4fad1ec5a9feabd8062caf8e2b65ae5ba2f
kros235 <kros235@naver.com>
2022-12-08 23:57:51 +0900

    2022-12-08
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/Main.java
+++ Coding_Test_Practice/src/Main.java
@@ -8,19 +8,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st          =   new StringTokenizer( br.readLine() );
-        int defend_rate             =   Integer.parseInt( st.nextToken() );
-        int defend_ignorance_rate   =   Integer.parseInt( st.nextToken() );
 
         StringBuilder sb = new StringBuilder();
-
-        if ( defend_ignorance_rate == 0 && defend_rate < 100)
-            sb.append(1);
-        else if ( defend_rate * (100 - defend_ignorance_rate)/100 >= 100)
-            sb.append(0);
-        else
-            sb.append(1);
-        bw.write(String.valueOf(sb));
+        bw.write("비와이");
 
         bw.flush();
         br.close();
