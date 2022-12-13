067b5d8a09f02e617379ae1e2a4f58b469601959
kros235 <kros235@naver.com>
2022-12-13 17:19:31 +0900

    2022-12-13
    Chapter #13 :
     - set_and_map

--- Coding_Test_Practice/src/Main.java
+++ Coding_Test_Practice/src/Main.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,15 +13,42 @@ public class Main {
 
 
         StringBuilder sb    =   new StringBuilder();
+        StringTokenizer st  =   new StringTokenizer( br.readLine() );
 
-        int round           =   Integer.parseInt( br.readLine() );
-        for ( int i = 0 ; i < round ; i++){
-            StringTokenizer st  =   new StringTokenizer( br.readLine() );
+        int set_N_index     =   Integer.parseInt( st.nextToken() );
+        int set_M_index     =   Integer.parseInt( st.nextToken() );
 
-            BigInteger a   =   new BigInteger( st.nextToken() );
-            BigInteger b   =   new BigInteger( st.nextToken() );
-            sb.append ( a.add( b ) + "\n");
+        String[] set_N     =   new String[set_N_index];
+        String[] set_M     =   new String[set_M_index];
+
+        for ( int i = 0 ; i < (set_N_index + set_M_index) ; i++){
+
+           if( i < set_N_index)
+               set_N[i] =  br.readLine() ;
+           else
+               set_M[i - set_N_index] =  br.readLine() ;
         }
+        Arrays.sort(set_M);
+
+        int count   =   0;
+
+        // count
+        for ( int i = 0 ; i < set_N_index ; i++){
+
+            for ( int j = 0 ; j < set_M_index ; j++){
+                if ( set_M[j].charAt(0) != set_N[i].charAt(0) ) continue;
+                else{
+                    if( set_M[j].equals( set_N[i] ) == true){
+                        count ++;
+                    }
+                }
+            }
+
+        }
+
+        sb.append(count + "\n");
+
+
 
         bw.write( String.valueOf(sb) );
 
