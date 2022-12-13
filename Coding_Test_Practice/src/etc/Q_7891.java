5041812ff4474d0b00a5fa65b600b10cb540dfce
kros235 <kros235@naver.com>
2022-12-13 11:19:06 +0900

    2022-12-13
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_7891.java
+++ Coding_Test_Practice/src/etc/Q_7891.java
@@ -1,8 +1,10 @@
+package etc;
+
 import java.io.*;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Q_7891 {
 
     public static void main(String args[]) throws IOException {
 
@@ -28,5 +30,5 @@ public class Main {
         bw.close();
 
     }
-}
 
+}
