a03b2030c9b4bc6f356995d51c9a009f160d1977
kros235 <kros235@naver.com>
2023-01-28 02:00:27 +0900

    2023-01-27
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_6840.java
+++ Coding_Test_Practice/src/etc/Q_6840.java
@@ -1,7 +1,9 @@
+package etc;
+
 import java.io.*;
 import java.util.Arrays;
 
-public class Main {
+public class Q_6840 {
 
     public static void main(String args[]) throws IOException {
 
@@ -23,6 +25,5 @@ public class Main {
         br.close();
         bw.close();
     }
+
 }
-
-
