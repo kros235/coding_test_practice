10eeb91fd28fd08c1b473ef7184691c780744f67
kros235 <kros235@naver.com>
2023-02-07 11:53:37 +0900

    2023-02-07
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_2161.java
+++ Coding_Test_Practice/src/etc/Q_2161.java
@@ -1,15 +1,18 @@
-import java.io.*;
-import java.util.*;
+package etc;
 
-public class Main {
+import java.io.*;
+import java.util.ArrayList;
+
+public class Q_2161 {
 
     public static void main(String args[]) throws IOException {
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int total_number =  Integer.parseInt(br.readLine());
-        ArrayList<Integer>  cards   =   new ArrayList<>();
+        int total_number = Integer.parseInt(br.readLine());
+        ArrayList<Integer> cards = new ArrayList<>();
 
         StringBuilder sb = new StringBuilder();
 
@@ -22,25 +25,24 @@ public class Main {
         // ============================================================
         // logic function
         // ============================================================
-        while ( cards.size() != 1 ){
+        while (cards.size() != 1) {
 
             // pop the top card
-            sb.append (cards.get(0) + " ");
+            sb.append(cards.get(0) + " ");
             cards.remove(0);
 
             // move card to the bottom
-            int temp   =    cards.get(0);
+            int temp = cards.get(0);
             cards.remove(0);
             cards.add(temp);
 
         }
-        sb.append( cards.get(0) );
+        sb.append(cards.get(0));
         bw.write(String.valueOf(sb));
 
         bw.flush();
         br.close();
         bw.close();
     }
+
 }
-
-
