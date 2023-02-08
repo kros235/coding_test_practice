16839a106c53a5cde0a4fc88b7556b237720e576
kros235 <kros235@naver.com>
2023-02-08 12:56:50 +0900

    2023-02-08
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_1380.java
+++ Coding_Test_Practice/src/etc/Q_1380.java
@@ -1,9 +1,17 @@
-import java.io.*;
-import java.util.*;
+package etc;
 
-public class Main {
+import java.io.*;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Q_1380 {
+
+
 
     public static void main(String args[]) throws IOException {
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -51,6 +59,5 @@ public class Main {
         br.close();
         bw.close();
     }
+
 }
-
-
