be95792342b8164f1386f8a5e805367a22f1fc9e
kros235 <kros235@naver.com>
2023-02-28 19:10:33 +0900

    2023-02-28
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_5426.java
+++ Coding_Test_Practice/src/etc/Q_5426.java
@@ -1,7 +1,11 @@
+package etc;
+
 import java.io.*;
 
-public class Main {
+public class Q_5426 {
+
     public static void main(String args[]) throws IOException {
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
