f33b6a886d0742cca6ad3d68df6242720b6137da
kros235 <kros235@naver.com>
2023-04-03 13:51:20 +0900

    2023-04-03
    Chapter #08 :
     - general_mathematics

--- Coding_Test_Practice/src/Main.java
+++ Coding_Test_Practice/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -8,12 +9,26 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        int round    =   Integer.parseInt( br.readLine() );
-        int squares  =   (int) Math.sqrt( Math.pow( 4, round ) );
-        int dots     =   (int) Math.pow ( squares + 1 , 2 );
-        sb.append( dots );
 
 
+        StringTokenizer st    =   new StringTokenizer( br.readLine() );
+        String line           =   st.nextToken();
+        int    num            =   Integer.parseInt( st.nextToken() );
+        long sum              =   0;
+
+        int count             =   0;
+        for ( int i = line.length()-1 ; i >=0 ; i-- ){
+            if ( line.charAt(i) >= 65 &&  line.charAt(i) <= 90){
+                int temp    =   line.charAt(i) - 55;
+                sum        +=   temp * Math.pow( num, count++);
+            }
+            else {
+                int temp = line.charAt(i) - 48;
+                sum        +=   temp * Math.pow( num, count++);
+            }
+        }
+
+        sb.append( sum );
         bw.write(sb + "\n");
         bw.flush();
         br.close();
---
+++ Coding_Test_Practice/src/general_mathematics_1/Q_2745.java
@@ -0,0 +1,41 @@
+package general_mathematics_1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Q_2745 {
+
+
+    public static void main(String args[]) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String line = st.nextToken();
+        int num = Integer.parseInt(st.nextToken());
+        long sum = 0;
+
+        int count = 0;
+        for (int i = line.length() - 1; i >= 0; i--) {
+            if (line.charAt(i) >= 65 && line.charAt(i) <= 90) {
+                int temp = line.charAt(i) - 55;
+                sum += temp * Math.pow(num, count++);
+            } else {
+                int temp = line.charAt(i) - 48;
+                sum += temp * Math.pow(num, count++);
+            }
+        }
+
+        sb.append(sum);
+        bw.write(sb + "\n");
+        bw.flush();
+        br.close();
+        bw.close();
+
+    }
+}
