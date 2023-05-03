9b8a172ada59ea487e1937740b70247e85a513a7
kros235 <kros235@naver.com>
2023-05-03 17:54:09 +0900

    2023-05-03
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_10797.java
+++ Coding_Test_Practice/src/etc/Q_10797.java
@@ -1,7 +1,12 @@
+package etc;
+
 import java.io.*;
+import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Q_10797 {
+
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,4 +27,5 @@ public class Main {
         br.close();
         bw.close();
     }
+
 }
