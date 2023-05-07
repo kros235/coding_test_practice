647ac7622b0872bc36a80327157f1ab79c30973d
kros235 <kros235@naver.com>
2023-05-08 00:20:03 +0900

    2023-05-07
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_2960.java
+++ Coding_Test_Practice/src/etc/Q_2960.java
@@ -1,8 +1,10 @@
+package etc;
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Q_2960 {
 
     public static int find_min( int[] numbers ){
 
