74b0b01d2621c1403309e65236c67407a395b42d
kros235 <kros235@naver.com>
2023-06-07 07:23:44 +0900

    2023-06-07
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_2490.java
+++ Coding_Test_Practice/src/etc/Q_2490.java
@@ -1,9 +1,12 @@
+package etc;
+
 import java.io.*;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String args[]) throws IOException {
+public class Q_2490 {
 
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
@@ -44,4 +47,5 @@ public class Main {
         br.close();
         bw.close();
     }
+
 }
