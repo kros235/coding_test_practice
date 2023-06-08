b5ace4eb84b40c8656fc3895325ad389412cddda
kros235 <kros235@naver.com>
2023-06-09 08:24:31 +0900

    2023-06-09
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_2506.java
+++ Coding_Test_Practice/src/etc/Q_2506.java
@@ -1,8 +1,12 @@
+package etc;
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Q_2506 {
+
+
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,4 +32,5 @@ public class Main {
         br.close();
         bw.close();
     }
+
 }
