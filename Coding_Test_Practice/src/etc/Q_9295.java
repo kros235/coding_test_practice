e9902554545df475cd844951462bd926aff1f684
kros235 <kros235@naver.com>
2023-06-19 03:41:17 +0900

    2023-06-18
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_9295.java
+++ Coding_Test_Practice/src/etc/Q_9295.java
@@ -1,9 +1,11 @@
+package etc;
+
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Q_9295 {
+
     public static void main(String args[]) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
