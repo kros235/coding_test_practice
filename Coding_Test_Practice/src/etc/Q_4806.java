2ed619528d7f002f17945403dd40c82431b6f70d
kros235 <kros235@naver.com>
2023-07-21 10:50:28 +0900

    2023-07-21
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_4806.java
+++ Coding_Test_Practice/src/etc/Q_4806.java
@@ -1,8 +1,10 @@
+package etc;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
-public class Main {
+public class Q_4806 {
+
+
 
     public static void main(String args[]) throws IOException {
 
@@ -22,6 +24,4 @@ public class Main {
         bw.close();
     }
 
-
 }
-
