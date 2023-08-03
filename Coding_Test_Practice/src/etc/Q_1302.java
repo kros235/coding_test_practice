9043ba6f1ecc5e92d73502592c5c5ab938ce2a5d
kros235 <kros235@naver.com>
2023-08-03 11:38:48 +0900

    2023-08-03
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_1302.java
+++ Coding_Test_Practice/src/etc/Q_1302.java
@@ -1,10 +1,12 @@
+package etc;
 
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Q_1302 {
 
     public static void main(String args[]) throws IOException {
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -52,7 +54,4 @@ public class Main {
         br.close();
         bw.close();
     }
-
-
 }
-
