1edc695a31367c616e24767b303d2e22361556fc
kros235 <kros235@naver.com>
2023-09-22 12:48:14 +0900

    2023-09-22
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_5800.java
+++ Coding_Test_Practice/src/etc/Q_5800.java
@@ -1,9 +1,13 @@
+package etc;
 
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Q_5800 {
+
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
