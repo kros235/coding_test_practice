2135198acb6408852b75e5e7eb5a49b1d8fbe3ce
kros235 <kros235@naver.com>
2023-10-10 13:46:54 +0900

    2023-10-10
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_1253.java
+++ Coding_Test_Practice/src/etc/Q_1253.java
@@ -1,8 +1,10 @@
+package etc;
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Q_1253 {
 
     public static void main(String args[]) throws IOException {
 
