2f8912e018b48d2eb46f62ed48017b3a1f601191
kros235 <kros235@naver.com>
2023-11-26 00:56:56 +0900

    2023-11-25
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_1758.java
+++ Coding_Test_Practice/src/etc/Q_1758.java
@@ -1,8 +1,12 @@
+package etc;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Q_1758 {
 
     public static void main(String args[]) throws IOException {
 
@@ -33,6 +37,4 @@ public class Main {
         br.close();
         bw.close();
     }
-
-
 }
