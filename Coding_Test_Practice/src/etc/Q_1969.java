f89fa6e38ae3edcc0f20d88dc32878b7c22a40ab
kros235 <kros235@naver.com>
2023-11-27 11:54:36 +0900

    2023-11-27
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_1969.java
+++ Coding_Test_Practice/src/etc/Q_1969.java
@@ -1,10 +1,15 @@
+package etc;
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Q_1969 {
 
     public static void main(String args[]) throws IOException {
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -63,4 +68,5 @@ public class Main {
         br.close();
         bw.close();
     }
+
 }
