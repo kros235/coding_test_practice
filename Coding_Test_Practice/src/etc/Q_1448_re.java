43e3ba73f4f033c64ab86557fd1f9b6400ff8775
kros235 <kros235@naver.com>
2023-12-01 11:34:37 +0900

    2023-12-01
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_1448_re.java
+++ Coding_Test_Practice/src/etc/Q_1448_re.java
@@ -1,11 +1,15 @@
+package etc;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
-public class Main {
-
+public class Q_1448_re {
 
     public static void main(String args[]) throws IOException {
+
 
         BufferedReader br           =   new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw           =   new BufferedWriter(new OutputStreamWriter(System.out));
@@ -33,4 +37,5 @@ public class Main {
         br.close();
         bw.close();
     }
+
 }
