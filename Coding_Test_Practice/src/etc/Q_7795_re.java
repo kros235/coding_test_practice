fb4b6aa9220ece721ea167e9895ba1b79e9ba647
kros235 <kros235@naver.com>
2024-01-01 22:32:01 +0900

    2024-01-01
    Chapter #etc :
     - etc

--- Coding_Test_Practice/src/etc/Q_7795_re.java
+++ Coding_Test_Practice/src/etc/Q_7795_re.java
@@ -1,12 +1,11 @@
-
-
+package etc;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
-
-
+public class Q_7795_re {
 
     public static void main(String args[]) throws IOException {
 
